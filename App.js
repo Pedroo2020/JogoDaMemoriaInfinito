3036a2c3f2cd9f00365ff5c26eccfaf003ae3d6d
JaumVictorr <victor21.jvl@gmail.com>
2025-09-02 11:10:13 -0300

    info

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import {NavigationContainer} from "@react-navigation/native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import FormScreen from "./screens/FormScreen";
+import { useFonts } from "expo-font";
+import { ActivityIndicator, View } from "react-native";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Lobster: require("./assets/fonts/Lobster-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <ActivityIndicator size="large" color="#8B0000" />
+            </View>
+        );
+    }
 
     const Stack = createNativeStackNavigator();
 
     return (
-      <NavigationContainer>
-          <Stack.Navigator >
-              <Stack.Screen options={{ headerShown: false }}
-                            name={"Form"}
-                            component={FormScreen} />
-
-              <Stack.Screen options={{ headerShown: false }}
-                        name={"Home"}
-                        component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    options={{ headerShown: false }}
+                    name="Form"
+                    component={FormScreen}
+                />
+                <Stack.Screen
+                    options={{ headerShown: false }}
+                    name="Home"
+                    component={HomeScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
