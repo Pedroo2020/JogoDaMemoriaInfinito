7c7193827f481026deedec06a3a5cf1a83502f96
Pedroo2020 <pedro.halo.ph@gmail.com>
2025-09-02 09:18:21 -0300

    add files

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import FormScreen from "./screens/FormScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const Stack = createNativeStackNavigator();
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen options={{ headerShown: false }}
+                        name={"Home"}
+                        component={HomeScreen} />
+
+          <Stack.Screen options={{ headerShown: false }}
+                        name={"Form"}
+                        component={FormScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+}
---
+++ design/colors.js
@@ -0,0 +1,10 @@
+export default {
+    white: "#FFFFFF",
+    black: "#000000",
+
+    redDark: "#980A24",
+    redBase: "#BF3131",
+
+    blueDark: "#0F386A",
+    blueBase: "#164886",
+}
